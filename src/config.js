7385ad7581de2ef1da51eb2c4d5c583911453f23
nagarajan091792 <87260947+nagarajan091792@users.noreply.github.com>
2022-12-03 17:48:49 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config={
-    api:"https://money-manage-node.herokuapp.com"
+    api:"money-manage-backend-r7fx-aetsb1lfv-nagarajan091792-gmailcom.vercel.app"
     // api:"http://localhost:5000"
 }
